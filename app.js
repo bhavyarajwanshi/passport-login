117652a85b1caf8646ba3f81e0971b79354c481b
bhavyarajwanshi <bhavyarajwanshi99@gmail.com>
2024-05-09 00:30:05 +0530

    welcome by ejs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.set('view engine', 'ejs');
 
 app.use("/",require('./routes/index'))
 app.use("/users",require('./routes/users'));
